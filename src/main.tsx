a5de9b90ccc68f61068fecffb9481b1e2c27ff17
YlanDecode <rabenaly3@gmail.com>
2025-05-18 10:27:54 +0300

    feat: auth

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,6 @@ import {
 import './index.css';
 import { Loader } from '@/components';
 import PaymentPage from "@/page/pricing/PaiementPage.tsx";
-import { PrivateRoute } from "@/page/routes/PrivateRoutes.tsx";
 
 
 const LandingPage = lazy(() => import('@/page/landing/Landing'));
@@ -36,22 +35,8 @@ createRoot(rootElement).render(
                     <Route path="/" element={<LandingPage />} />
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/payment" element={<PaymentPage />} />
-                    <Route
-                        path="/admin"
-                        element={
-                            <PrivateRoute requiredRole="admin">
-                                <AdminPage />
-                            </PrivateRoute>
-                        }
-                    />
-                    <Route
-                        path="/profile"
-                        element={
-                            <PrivateRoute requiredRole="user">
-                                <ProfilePage />
-                            </PrivateRoute>
-                        }
-                    />
+                    <Route path="/admin" element={<AdminPage />} />
+                    <Route path="/profile" element={<ProfilePage />} />
                     <Route path="*" element={<Navigate to="/" />} />
                 </Routes>
             </Suspense>
