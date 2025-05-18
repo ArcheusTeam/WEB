c4bb79b3d46e770f4a30254235e8ac24d3295d84
YlanDecode <rabenaly3@gmail.com>
2025-05-18 03:08:13 +0300

    fix: resolve conflict

--- src/page/admin/page.tsx
+++ src/page/admin/page.tsx
@@ -7,7 +7,7 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 
 import data from "./data.json"
 
-export default function Page() {
+export function Admin() {
   return (
     <SidebarProvider>
       <AppSidebar variant="inset" />
